3ebe3779dd3ac9d42078cc59a74b5d6ce9f86b3a
Vinello28 <74827522+Vinello28@users.noreply.github.com>
2024-03-30 19:57:13 +0100

    Exercice 2.3, updated with some sec

--- Exercises/Esercizio 1/UDP_Client.py
+++ Exercises/Esercizio 1/UDP_Client.py
@@ -7,6 +7,11 @@ serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_DGRAM)
 message = input("Input lowercase sentence: ")
 clientSocket.sendto(message.encode(), (serverName, serverPort))
-consonants, serverAddress = clientSocket.recvfrom(2048)
-print(f"Received message: {consonants.decode()} from {serverAddress}")
-clientSocket.close()
+clientSocket.settimeout(5)
+try:
+    consonants, serverAddress = clientSocket.recvfrom(2048)
+    print(f"Received message: {consonants.decode()} from {serverAddress}")
+except timeout:
+    print("The server is not responding")
+finally:
+    clientSocket.close()
