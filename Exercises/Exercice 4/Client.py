7d6d3bf533a5aa4d7ed854d0ad0e253245dc1715
Gabriele Vianello <vianello.tech@gmail.com>
2024-04-03 08:39:34 +0200

    esercizio 2

---
+++ Exercises/Exercice 4/Client.py
@@ -0,0 +1,15 @@
+from socket import *
+
+serverName = 'localhost'
+serverPort = 12000
+clientSocket = socket(AF_INET, SOCK_STREAM)
+clientSocket.connect((serverName, serverPort))
+
+while True:
+    sentence = input('Input lowercase sentence:')
+    clientSocket.send(sentence.encode())
+    if sentence == '.':
+        break
+    modifiedSentence = clientSocket.recv(1024)
+    print('From Server:', modifiedSentence.decode())
+clientSocket.close()
